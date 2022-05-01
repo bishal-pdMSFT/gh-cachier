e940f07a8f4b5524b81ae9615cc29c7d2530fbb4
Bishal Prasad <bishal-pdmsft@github.com>
2022-05-01 22:57:16 +0530

    repo and org scope cache usages

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/cli/go-gh v0.0.3
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/pflag v1.0.5
 )
--- main.go
+++ main.go
@@ -1,68 +1,114 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/cli/go-gh"
-
 	flag "github.com/spf13/pflag"
 )
 
+type RepoCacheUsage struct {
+	FullName                string `json:"full_name"`
+	ActiveCachesSizeInBytes int64  `json:"active_caches_size_in_bytes"`
+	ActiveCachesCount       int32  `json:"active_caches_count"`
+}
+
+type OrgCacheUsage struct {
+	TotalCachesSize   int64 `json:"total_active_caches_size_in_bytes"`
+	ActiveCachesCount int32 `json:"total_active_caches_count"`
+}
+
+type OrgCacheUsageByRepo struct {
+	TotalCount      int32            `json:"total_count"`
+	RepoCacheUsages []RepoCacheUsage `json:"repository_cache_usages"`
+}
+
+type Scope interface {
+	getCacheUsage()
+	getCacheUsageEndpoint() string
+}
+
+type RepoScope struct {
+	Name       string
+	CacheUsage RepoCacheUsage
+}
+
+type OrgScope struct {
+	Name             string
+	IsUsageByRepo    bool
+	CacheUsage       OrgCacheUsage
+	CacheUsageByRepo OrgCacheUsageByRepo
+}
+
+func (r *RepoScope) getCacheUsage() {
+	fmt.Println("Getting Actions cache usage for repo: " + r.Name)
+	fetchCacheUsageFromGitHub(r.getCacheUsageEndpoint(), &r.CacheUsage)
+	prettyPrint(r.CacheUsage)
+}
+
+func (r *RepoScope) getCacheUsageEndpoint() string {
+	return fmt.Sprintf("repos/%s/actions/cache/usage", r.Name)
+}
+
+func (o *OrgScope) getCacheUsage() {
+	fmt.Println("Getting Actions cache usage for organization: " + o.Name)
+
+	if o.IsUsageByRepo {
+		fetchCacheUsageFromGitHub(o.getCacheUsageEndpoint(), &o.CacheUsageByRepo)
+		prettyPrint(o.CacheUsageByRepo)
+		return
+	}
+
+	fetchCacheUsageFromGitHub(o.getCacheUsageEndpoint(), &o.CacheUsage)
+	prettyPrint(o.CacheUsage)
+}
+
+func (o *OrgScope) getCacheUsageEndpoint() string {
+	if o.IsUsageByRepo {
+		return fmt.Sprintf("orgs/%s/actions/cache/usage-by-repository", o.Name)
+	}
+
+	return fmt.Sprintf("orgs/%s/actions/cache/usage", o.Name)
+}
+
 func main() {
-	// fmt.Println("hi world, this is the gh-cachier extension!")
-	// client, err := gh.RESTClient(nil)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// response := struct {Login string}{}
-	// err = client.Get("user", &response)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Printf("running as %s\n", response.Login)
-
 	var repo *string = flag.String("repo", "", "Name of the repository to fetch cache usage for")
-	flag.Parse()
+	var org *string = flag.String("org", "", "Name of the organization to fetch cache usage for")
+	//var ent *string = flag.String("enterprise", "", "Name of the enterprise to fetch cache usage for")
+	var byRepo *bool = flag.BoolP("by-repo", "R", false, "Flag to fetch cache usage per repo")
 
+	flag.Parse()
 	args := flag.Args()
-	// for i, arg := range args {
-	// 	fmt.Printf("arg %d - %s", i, arg)
-	// }
-	//
-	// fmt.Println("repo: " + *repo)
 
 	if args[0] == "usage" {
-		getRepoCacheUsage(repo)
+		if *repo != "" {
+			var repoScope RepoScope = RepoScope{Name: *repo}
+			repoScope.getCacheUsage()
+		} else if *org != "" {
+			var orgScope OrgScope = OrgScope{Name: *org, IsUsageByRepo: *byRepo}
+			orgScope.getCacheUsage()
+		}
 	}
 
 }
 
-// usage <owner/repo>
-func getRepoCacheUsage(repo *string) {
+func fetchCacheUsageFromGitHub(endpoint string, response interface{}) {
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	response := struct {
-		full_name                   string
-		active_caches_size_in_bytes int64
-		active_caches_count         int32
-	}{}
-
-	fmt.Println("repo: " + *repo)
-
-	endpoint := fmt.Sprintf("repos/%s/actions/cache/usage", *repo)
-	fmt.Println("endpoint: " + endpoint)
 	err = client.Get(endpoint, &response)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(response)
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func prettyPrint(i interface{}) {
+	fmt.Println()
+	s, _ := json.MarshalIndent(i, "", "\t")
+	fmt.Println(string(s))
+	fmt.Println()
+}
